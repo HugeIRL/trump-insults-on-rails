3d34f6e7b1526b9790e6b6c5be3db92b7120087d
HugeIRL <codepirate@icloud.com>
2021-02-16 05:33:40 -0600

    bootstrap upgrade and styling corrections

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
+require("packs/custom")
+require("packs/posts.js")
 
 Rails.start()
 Turbolinks.start()

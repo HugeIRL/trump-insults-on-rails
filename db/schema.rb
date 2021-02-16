0f0f6e004a0bb7dcb23a283026acea5bd01378e6
HugeIRL <codepirate@icloud.com>
2021-02-16 04:37:57 -0600

    add csv file and seed the db

---
+++ db/schema.rb
@@ -0,0 +1,40 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_02_16_102846) do
+
+  create_table "insults", force: :cascade do |t|
+    t.string "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "targets", force: :cascade do |t|
+    t.string "prey"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.datetime "date"
+    t.string "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "target_id", null: false
+    t.integer "insult_id", null: false
+    t.index ["insult_id"], name: "index_tweets_on_insult_id"
+    t.index ["target_id"], name: "index_tweets_on_target_id"
+  end
+
+  add_foreign_key "tweets", "insults"
+  add_foreign_key "tweets", "targets"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+Tweet.delete_all
+Target.delete_all
+Insult.delete_all
+
+filename = Rails.root.join("db/trump_tweets.csv")
+
+puts "Importing CSV data from: #{filename}"
+
+csv_data = File.read(filename)
+tweets = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+tweets.each do |t|
+  target = Target.find_or_create_by(prey: t["target"])
+  insult = Insult.find_or_create_by(text: t["insult"])
+
+  if target && target&.valid?
+    if insult && insult&.valid?
+      tweet = Tweet.create(
+        date:    t["date"],
+        message: t["tweet"],
+        target:  target,
+        insult:  insult
+      )
+      puts "Invalid tweet #{t['tweet']}" unless tweet&.valid?
+    else
+      puts "Invalid insult: #{t['insult']} for tweet: #{t['tweet']}"
+    end
+  else
+    puts "Invalid target: #{t['target']} for tweet: #{t['tweet']}"
+  end
+end
+
+puts "Created #{Tweet.count} tweets."
+puts "Created #{Target.count} targets."
+puts "Created #{Insult.count} insults."
